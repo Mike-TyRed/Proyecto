fcd7b27472bec0b768e27e5029dd28ff5b49c815
Miguel T. Rojo <miguel.terrazas193@tectijuana.edu.mx>
2022-04-28 08:30:29 -0700

    admin3

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ do                                                  //Sistema
 {
     Console.WriteLine("----Menu de acciones-----");
     Console.WriteLine("[1] Crear nueva B.D");
-    Console.WriteLine("[2] Borar B.D existente");
+    Console.WriteLine("[2] Borrar B.D existente");
     Console.WriteLine("[3] Muestra todas las B.D");
     Console.WriteLine("[4] Usar B.D existente");
     Console.WriteLine("[5] Finalizar programa");
@@ -24,12 +24,15 @@ do                                                  //Sistema
             break;
 
         case 2:                                     //Borrar base de datos
+            BorrarBD();
             break;
 
         case 3:                                     //Mostrar bases de datos
+            MostrarBD();
             break;
 
         case 4:                                     //Usar base de datos
+            UsarBD();
             break;
 
         case 5:                                     //Finalizar programa
@@ -43,17 +46,30 @@ do                                                  //Sistema
     }
 } while (opcion != 5);
 
-static void CrearBD()
+static void CrearBD()                               //Método creación base de datos (carpeta)
 {
     Console.Write("Ingresa el nombre de la base de datos: ");
     string name = Console.ReadLine();
 
-    string path = @"C:\Users\Mike\Documents\GitHub\Proyecto\" + name;
+    string path = Directory.GetCurrentDirectory() + @"\" + name;
 
+    Console.Clear();
     if (!Directory.Exists(path))
     {
         Directory.CreateDirectory(path);
-        Console.WriteLine("Base de datos" + name + "creada");
+        Console.Write("Base de datos " + name + " creada");
         Console.ReadLine();
     }
+}
+
+static void BorrarBD(){                             //Método borrar base de datos
+
+}
+
+static void MostrarBD(){                            //Método mostrar base de datos
+
+}
+
+static void UsarBD(){                               //Método usar base de datos
+
 }
